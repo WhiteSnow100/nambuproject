1447c1339a8a5408f75df4ccfa511a640f5cc59b
WhiteSnow100 <bsmj.100@gmail.com>
2024-11-20 14:28:33 +0900

    add file - bsmj

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+// . login한 경우 : Home/LogOut
+// . login안 한 경우1 : Home / LogIn / 가입하기 
+// . Home/login/가입하기 : 각 화면 링크
+// . LogOut : 사용자초기화하고 Home 링크
+
+function Header(){
+    return <div> </div>
+}
