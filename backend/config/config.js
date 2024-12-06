491f5d650bab14a848cc5a7361c0e3407d99421b
WhiteSnow100 <bsmj.100@gmail.com>
2024-12-06 15:45:22 +0900

    commit

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,21 +2,21 @@ require('dotenv').config();
 
 const config = {
   development: {
-    username: process.env.DB_USERNAME,
+    username: process.env.MY_DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
   test: {
-    username: process.env.DB_USERNAME,
+    username: process.env.MY_DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USERNAME,
+    username: process.env.MY_DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
